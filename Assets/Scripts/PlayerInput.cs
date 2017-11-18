8311a086a7a1fea2aba73f5ddeff62e90362bf9a
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-11-18 20:36:49 +0100

    added trajectory draw

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -13,6 +13,23 @@ public class PlayerInput : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
     public float force, maxMagnitude;
 
     private float magnitude;
+    private Rigidbody rb;
+    public LineRenderer line;
+
+    private bool pressed;
+
+    void Start()
+    {
+        rb = player.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (pressed)
+        {
+            DrawTrajectory();
+        }
+    }
 
     public void OnDrag(PointerEventData eventData)
     {
@@ -36,20 +53,39 @@ public class PlayerInput : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
         direction = direction.normalized;
 
         Debug.DrawRay(player.transform.position, direction, Color.red);
-        Debug.Log("Magnitude: " + magnitude);
+        //Debug.Log("Magnitude: " + magnitude);
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         startScreenPos = eventData.position;
-
+        pressed = true;
+        line.gameObject.SetActive(true);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Rigidbody rb = player.GetComponent<Rigidbody>();
         rb.angularVelocity = Vector3.zero;
         rb.velocity = Vector3.zero;
         rb.AddForce(direction * magnitude * force);
+        pressed = false;
+        line.gameObject.SetActive(false);
+    }
+
+    void DrawTrajectory()
+    {
+        int vert = 20;
+        line.positionCount = vert;
+
+        Vector3 pos = player.transform.position;
+        Vector3 vel = ((direction * magnitude * force) / rb.mass) * Time.fixedDeltaTime;
+        Vector3 grav = Physics.gravity;
+
+        for (int i = 0; i < vert; i++)
+        {
+            line.SetPosition(i, pos);
+            vel += grav * Time.fixedDeltaTime;
+            pos += vel * Time.fixedDeltaTime;
+        }
     }
 }
