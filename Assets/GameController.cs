7bf8f6e67420fcaa002ec8a0a022a2721ba6084c
DESKTOP-HBKHA3T\Tomek <ghost744@gmail.com>
2017-11-19 00:26:39 +0100

    added wall movement

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-    public int BallsToCollect = 3;
+    public int BallsToCollect = 2;
     int currentBalls = 0;
 
 	// Use this for initialization
@@ -14,7 +14,7 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(BallsToCollect >= 5)
+		if(currentBalls >= BallsToCollect)
         {
             Debug.Log("Win");
         }
--- Assets/Scripts/WallMovement.cs
+++ Assets/Scripts/WallMovement.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WallMovement : MonoBehaviour {
+    enum MovementDirection
+    {
+        Forward,
+        Backward
+    }
 
-    public float MovementLength = 5.0f;
+    MovementDirection movDir = MovementDirection.Forward;
+    public float MovementSpeed = 0.1f;
+    public float MovementLength = 0.1f;
     float offset = 0f;
 	// Use this for initialization
 	void Start () {
@@ -13,7 +20,23 @@ public class WallMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        offset += Time.deltaTime;
-        //transform.position = 
+
+        switch (movDir)
+        {
+            case MovementDirection.Forward:
+                offset += MovementSpeed * Time.deltaTime;
+                if (offset >= MovementLength)
+                    movDir = MovementDirection.Backward;
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + offset);
+                break;
+            case MovementDirection.Backward:
+                offset -= MovementSpeed * Time.deltaTime;
+                if (offset <= 0)
+                    movDir = MovementDirection.Forward;
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - offset);
+                break;
+            default:
+                break;
+        }
 	}
 }
