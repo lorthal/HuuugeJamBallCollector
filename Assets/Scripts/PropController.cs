aea164fff81b74b678871df2ae8a46b099b433ba
DESKTOP-HBKHA3T\Tomek <ghost744@gmail.com>
2017-11-18 21:34:08 +0100

    added wiatrak rotation

--- Assets/Scripts/PropController.cs
+++ Assets/Scripts/PropController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PropController : MonoBehaviour {
 
     public float RotationSpeed = 1.0f;
-    
+    public Vector3 dir;
 	void Update () {
-        this.gameObject.transform.Rotate(Vector3.right, Time.deltaTime * 50.0f * RotationSpeed);
+        this.gameObject.transform.Rotate(dir, Time.deltaTime * 50.0f * RotationSpeed);
 	}
 }
