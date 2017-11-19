a30082c3ab68146c178008edcca986f4f08c61a6
DESKTOP-HBKHA3T\Tomek <ghost744@gmail.com>
2017-11-19 12:48:56 +0100

    added pacman leave

--- Assets/Scripts/WinTriggerController.cs
+++ Assets/Scripts/WinTriggerController.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class WinTriggerController : MonoBehaviour {
 
+    bool isFinish = false;
+    float timer = 0;
     public GameObject WinPanel;
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            WinPanel.SetActive(true);
+            isFinish = true;
+
+        }
+    }
+
+    private void Update()
+    {
+        if(isFinish)
+        {
+            timer += Time.deltaTime;
+            if(timer >= 1.0f)
+            {
+                GetComponent<Animator>().enabled = false;
+                this.gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 5.0f * Time.deltaTime, gameObject.transform.position.z);
+                //timer = 0;
+            }
+
+            if( gameObject.transform.position.y >= 5f)
+                WinPanel.SetActive(true);
         }
     }
 }
