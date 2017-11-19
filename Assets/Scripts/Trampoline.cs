2c65739cbafb37dae9006f844e3449ea4326b03d
DESKTOP-HBKHA3T\Tomek <ghost744@gmail.com>
2017-11-19 13:39:20 +0100

    tramp

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float BounceStrength = 500.0f;
+    private void Start()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, BounceStrength, 0));
+        }
+    }
+
 }
