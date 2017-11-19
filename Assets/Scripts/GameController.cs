20f24ea30af7fe6e62d5e7d92dccb59e674bcbe9
DESKTOP-HBKHA3T\Tomek <ghost744@gmail.com>
2017-11-19 06:56:42 +0100

    added pacman animation and movement

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,7 @@ public class GameController : MonoBehaviour
 {
 
     public GameObject centerKiller, centerWinner;
+    Vector3 targetPackmanLocation = new Vector3(0.1f, 1.9f, 0f);
 
     public int BallsToCollect = 4;
     int currentBalls = 0;
@@ -18,6 +19,11 @@ public class GameController : MonoBehaviour
 		    done = true;
             centerKiller.SetActive(false);
             centerWinner.SetActive(true);
+        }
+        if(done)
+        {
+            if (centerWinner.transform.position.y <= targetPackmanLocation.y) 
+                centerWinner.transform.position = new Vector3(centerWinner.transform.position.x, centerWinner.transform.position.y + Time.deltaTime, centerWinner.transform.position.z);
         }
 	}
 
